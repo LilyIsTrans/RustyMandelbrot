e950f5ff655b9327b4a230152bcbf688cdd7b38a
Rilazy <35211613+Rilazy@users.noreply.github.com>
2023-04-06 23:55:39 -0400

    Define interface to UI

--- src/interface.rs
+++ src/interface.rs
@@ -1,17 +1,29 @@
 use num::complex::Complex;
+use nalgebra::{Vector2, Similarity2};
+use image::{Rgb32FImage, ColorType::Rgb32F};
+type Point = Complex<f64>;
 
-fn apply_transform() {
-    let scalar = 1;
 
-    
+pub struct Viewport {
+    /// The point in the center of the viewport
+    center: Point,
+    /// The radius of the largest circle centered at center which is entirely contained in the viewport
+    radius: f64
+}
 
-    let matrix = arr2(&[
-                                                [1, -3],
-                                                [2, 4 ]]);
+pub struct Frame {
+    viewport: Viewport,
+    buffer: Rgb32FImage
+}
 
-    let new_vector: Array1<_> = scalar * vector;
-    println!("{}", new_vector);
 
-    let new_matrix = matrix.dot(&new_vector);
-    println!("{}", new_matrix);
+
+impl Viewport {
+    pub fn default() -> Viewport {
+        Viewport { center: (Point {re: 0f64, im: 0f64}), radius: (2.0f64) }
+    }
+
+    pub fn new(center: Point, radius: f64) -> Viewport {
+        Viewport { center: (center), radius: (radius) }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use num::complex::Complex;
-
+mod interface;
 type Point = Complex<f64>;
 
 fn main() {
